c1a61efa41a27626ff857afc17a8056b98982fc9
jayashreemore <jayashreematre@gmail.com>
2024-02-23 21:12:12 -0500

    solution 3 for FizzBuzz worked finally at end

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -5,18 +5,17 @@
 // If a number is not divisible by either 3 or 5, log the number.
 
 
-// for (var i=1; i <= 100; i++)
-// {
-    
-//     // else if (i % 3 == 0)
-//         console.log("Fizz");
-//     else if (i % 5 == 0)
-//         console.log("Buzz");
-//     else
-//         console.log(i);
-//         if (i % 15 == 0)
-//         console.log("FizzBuzz");
-// }
+for (var i = 1; i <= 100; i++) {
+    if (i % 15 == 0)
+        console.log("Number " + i + " is FizzBuzz");
+    else if (i % 3 == 0)
+        console.log("Number " + i + " is Fizz");
+    else if (i % 5 == 0)
+        console.log("Number " + i + " is Buzz");
+    else
+        console.log(i);
+
+}
 
 // for(i=0; i<=100;i++){
 //     if(i/3==0){
@@ -25,32 +24,42 @@
 //     else if( i%5==0 && i/3!==0){
 //     console.log("Buzz");
 //     }
-    
+
 //     }
 
-   /* solution 1---------
-   
-   for (let x = 1; x <= 100; x++) {
+/* solution 1---------
+ 
+for (let x = 1; x <= 100; x++) {
 
-        //Filter out the numbers indivisible by either 3 and 5
-        if (!(x % 3 == 0) && !(x % 5 == 0)) console.log(x);
-        //Filter out numbers divisible by 3 and 5
-        else if ((x % 3 == 0) && (x % 5 == 0)) console.log("FizzBuzz");
-        //Filter out numbers divisible by 3
-        else if (x % 3 == 0) console.log("Fizz");
-        //Filter out numbers divisible by 5
-        else if (x % 5 == 0) console.log("Buzz");
-        }
-        */
+     //Filter out the numbers indivisible by either 3 and 5
+     if (!(x % 3 == 0) && !(x % 5 == 0)) console.log(x);
+     //Filter out numbers divisible by 3 and 5
+     else if ((x % 3 == 0) && (x % 5 == 0)) console.log("FizzBuzz");
+     //Filter out numbers divisible by 3
+     else if (x % 3 == 0) console.log("Fizz");
+     //Filter out numbers divisible by 5
+     else if (x % 5 == 0) console.log("Buzz");
+     }
+     */
+// -----------------------
 
-        for (let i=1; i <=100; i++){
-            if(i  %  3 === 0  && 1 % 5 === 0){
-             console.log('FizzBuzz')
-           }else if(i  %  3 === 0){
-             console.log('Fizz')
-           }else if(i % 5 == 0){
-             console.log('Buzz')
-           }else{
-            console.log(i)
-           }
-         }
+//Solution 2----------
+/*
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && 1 % 5 === 0) {
+        console.log('FizzBuzz')
+    }
+
+    else if (i % 3 === 0) {
+        console.log('Fizz')
+    }
+    else if (i % 5 == 0) {
+        console.log('Buzz')
+    }
+
+    else {
+        console.log(i)
+    }
+}*/
+
+//-------------------
