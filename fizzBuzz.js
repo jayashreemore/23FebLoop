3f415ca48382f444abab9c08e50711cba2ce2adb
jayashreemore <jayashreematre@gmail.com>
2024-02-23 20:58:33 -0500

    2nd solution for FizzBuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -28,7 +28,9 @@
     
 //     }
 
-    for (let x = 1; x <= 100; x++) {
+   /* solution 1---------
+   
+   for (let x = 1; x <= 100; x++) {
 
         //Filter out the numbers indivisible by either 3 and 5
         if (!(x % 3 == 0) && !(x % 5 == 0)) console.log(x);
@@ -39,3 +41,16 @@
         //Filter out numbers divisible by 5
         else if (x % 5 == 0) console.log("Buzz");
         }
+        */
+
+        for (let i=1; i <=100; i++){
+            if(i  %  3 === 0  && 1 % 5 === 0){
+             console.log('FizzBuzz')
+           }else if(i  %  3 === 0){
+             console.log('Fizz')
+           }else if(i % 5 == 0){
+             console.log('Buzz')
+           }else{
+            console.log(i)
+           }
+         }
