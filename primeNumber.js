460ecf691ac5e490062fd529199ed4c9fc26aa00
jayashreemore <jayashreematre@gmail.com>
2024-02-25 21:10:42 -0500

    This is second try of Prime number Part 1 of  JS Lab Java loops

--- primeNumber.js
+++ primeNumber.js
@@ -10,24 +10,24 @@ let test = number + 1;
 //}
 
 // check if number is greater than 1
-    // looping through 2 to number-1
-    for(let j = test ; j < test * 2 ; j++){
-        console.log("value of j is : " + j);
+// looping through 2 to number-1
+for (let j = test; j < test * 2; j++) {
+    console.log("value of j is : " + j);
     //    for (let i = 2; i < j; i++) {
     //        if (j % i == 0) {
     //            isPrime = false;
     //            break;
     //        }
     //    }
-    }   
-    if (isPrime) {
-        console.log(`${number} is a prime number`);
-    } else {
-        console.log(`${number} is a not prime number`);
-    }
+}
+if (isPrime) {
+    console.log(`${number} is a prime number`);
+} else {
+    console.log(`${number} is a not prime number`);
+}
 
 
 // check if number is less than 1
-else {
+ {
     console.log("The number is not a prime number.");
 }
